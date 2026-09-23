75043b9dfc510df5acaa4fdc26a5d23985f3b0e5
Brendon Taylor <brendon.taylor@monash.edu>
2026-09-23 15:47:31 +1000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet publish -c Release -r linux-x64 --self-contained true -o fornite-replay-parser
+dotnet publish FortniteReplayParser.csproj -c Release -r linux-x64 --self-contained true -o fornite-replay-parser
